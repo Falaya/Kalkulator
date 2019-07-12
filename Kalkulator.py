f843c517d42d92ed27e4288a9c61354b82f9ea07
Falaya <31096757+Falaya@users.noreply.github.com>
2019-07-12 11:06:38 +0200

    Update Kalkulator.py

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,4 +1,4 @@
-#kalkulator 0.1
+#kalkulator -0.1
 #Wyświetlacz na 10 liczb, dodawanie, odejmowanie, mnożenie i dzielenie.
 
 from tkinter import *
@@ -12,13 +12,73 @@ class Application(Frame):
 
     def create_widgets(self):
         """Tworzenie przycisków i wyświetlacza."""
-        self.wyswietlacz = Text(self, width = 30, height = 5, wrap = WORD)
-        self.secret_txt.grid(row = 1, column = 1, columnspan = 2, sticky = W)
-        
-        
+        #Oddzielenie nazwy kalkulatora od górnej krawędzi okna głównego
+        self.przerwa1 = Label(self,font=("Courier", 2) )
+        self.przerwa1.grid(row = 0, column = 0, sticky = W)
 
+        #Model kalulatora
+        self.nazwa = Label(self, text = "STANIO 7260",font=("Courier", 18) )
+        self.nazwa.grid(row = 1, column = 0, sticky = W)
+        
+        #Oddzielenie nazwy kalkulatora od górnej krawędzi wyświetlacza
+        self.przerwa2 = Label(self,font=("Courier", 4) )
+        self.przerwa2.grid(row = 2, column = 0, sticky = W)
+
+        #Wyświetlacz
+        self.wyswietlacz = Text(self, width = 11, height = 1, wrap = W, font=("Courier", 50))
+        self.wyswietlacz.grid(row = 3, column = 0, columnspan = 2, rowspan = 2, sticky = W)
+
+        #Oddzielenie górnej krawędzi wyświetlacza od przycisków
+        self.przerwa2 = Label(self,font=("Courier", 4) )
+        self.przerwa2.grid(row = 5, column = 0, sticky = W)
+
+        #Przycisk C - czyszczenie wszystkiego
+
+        #Przycisk <- - usuwanie ostatniego znaku
+
+        #Przycisk 1 - wprowadzenie liczby 1
+
+        #Przycisk 2 - wprowadzenie liczby 2
+       
+        #Przycisk 3 - wprowadzenie liczby 3
+
+        #Przycisk 4 - wprowadzenie liczby 4
+
+        #Przycisk 5 - wprowadzenie liczby 5
+
+        #Przycisk 6 - wprowadzenie liczby 6
+
+        #Przycisk 7 - wprowadzenie liczby 7
+
+        #Przycisk 8 - wprowadzenie liczby 8
+
+        #Przycisk 9 - wprowadzenie liczby 9
+
+        #Przycisk 0 - wprowadzenie liczby 0
+
+        #Przycisk +/- - zmiana znaku liczby w wyświetlaczu
+        
+        #Przycisk + - dodawanie
+
+        #Przycisk - - odejmowanie
+
+        #Przycisk * - mnożenie
+
+        #Przycisk / - dzielenie
+
+        #Przycisk . - stawianie kropki
+
+        #Przycisk = - wykonanie operacji
+
+        #Przycisk ( - wstawienie lewego nawiasu
+
+        #Przycisk ) - wstawienie prawego nawiasu
+
+        
 root = Tk()
 root.title("Kalkulator")
-root.geometry("800x600")
+root.geometry("440x600")
+
+app = Application(root)
 
 root.mainloop()
